752b65ccd54b6c356e8463c9362f85d5f75c032a
akshayshahane <akshay.shahane13@gmail.com>
2017-08-06 23:28:55 +0530

    fix : wrong buildConfig import in application class it was preventing log from printing

--- app/src/main/java/com/example/android/popularmoviesstageone/PopularMoviesApplication.java
+++ app/src/main/java/com/example/android/popularmoviesstageone/PopularMoviesApplication.java
@@ -1,8 +1,5 @@
 package com.example.android.popularmoviesstageone;
 
-import java.util.Timer;
-
-import timber.log.BuildConfig;
 import timber.log.Timber;
 
 /**
@@ -16,7 +13,12 @@ public class PopularMoviesApplication extends android.app.Application {
     public void onCreate() {
         super.onCreate();
         if (BuildConfig.DEBUG) {
-            Timber.plant(new Timber.DebugTree());
+            Timber.plant(new Timber.DebugTree() {
+                @Override
+                protected String createStackElementTag(StackTraceElement element) {
+                    return super.createStackElementTag(element) + ":" + element.getLineNumber();
+                }
+            });
 
         }
     }
